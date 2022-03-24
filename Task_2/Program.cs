ee42863e20cb2a225efff962d8cd1a493855feb9
Xenia <listk98@yandex.ru>
2022-03-25 02:29:01 +0300

    Add Task_2

---
+++ Task_2/Program.cs
@@ -0,0 +1,34 @@
+﻿// Напишите программу, которая принимает на вход число и выдает сумму цифр в числе
+
+int N = Prompt("Введите натуральное число: ");
+
+if (N > 0)
+{
+    Sum();
+}
+
+else Console.WriteLine("Число не является натуральным");
+
+
+int Prompt(String message)
+{
+    Console.Write(message);
+    string strValue = Console.ReadLine();
+    int Value = int.Parse(strValue);
+    return Value;
+}
+
+void Sum()
+{
+    int sum = 0;
+    string strArray = N.ToString();
+    char[] arr = strArray.ToCharArray();
+    for(int i = 0; i < strArray.Length; i++)
+    {
+        sum += Convert.ToInt32(strArray.Substring(i,1));
+    }
+    Console.WriteLine($"{sum}");
+}
+
+
+
